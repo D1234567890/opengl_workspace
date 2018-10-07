03395c8b85323f9d507faacfd5f9772d5f43b795
D1234567890 <league@WTS-IM-6.attlocal.net>
2018-10-07 16:01:51 -0700

    OpenGL Experimentation
    DONE

--- main.cpp
+++ main.cpp
@@ -1,11 +1,37 @@
+#define STB_IMAGE_IMPLEMENTATION
+
 #include "SDL2/SDL.h"
+#include "OpenGL/gl.h"
+#include "stb_image.h"
 
 int main(int argc, char** argv){
-    SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window* window = SDL_CreateWindow("OpenGL", 100, 100, 900, 500, 0);
 
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window* window = SDL_CreateWindow("OpenGL", 100, 100, 500, 500, SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL);
+    SDL_GLContext context = SDL_GL_CreateContext(window);
     SDL_Event event;
+
+    glClearColor(0, 0, 0, 0);
+
     bool isRunning = true;
+
+    //unsigned char texData[] = {255, 0, 0, 255, 0, 255, 0, 255, 0, 0, 255, 255, 0, 0, 255, 255, 255, 0, 0, 255, 0, 255, 0, 255, 0, 255, 0, 255, 0, 0, 255, 255, 255, 0, 0, 255};
+
+    int w, h, n;
+    unsigned char *texData = stbi_load("rat.jpg", &w, &h, &n, 0);
+    unsigned int textureID;
+
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 3, 3, 0, GL_RGBA, GL_UNSIGNED_BYTE, texData);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glEnable(GL_TEXTURE_2D);
+
+
     while(isRunning){
         while(SDL_PollEvent(&event)){
             switch(event.type){
@@ -20,8 +46,30 @@ int main(int argc, char** argv){
                 }
             }
         }
+
+        glClear(GL_COLOR_BUFFER_BIT);
+        glEnable(GL_MULTISAMPLE);
+
+        glBegin(GL_TRIANGLES);
+        glTexCoord2f(0, 1);
+        glVertex2f(-1, 1);
+        glTexCoord2f(1, 1);
+        glVertex2f(1, 1);
+        glTexCoord2f(0, 0);
+        glVertex2f(-1, -1);
+        glTexCoord2f(0, 0);
+        glVertex2f(-1, -1);
+        glTexCoord2f(1, 0);
+        glVertex2f(1, -1);
+        glTexCoord2f(1, 1);
+        glVertex2f(1, 1);
+        
+        glEnd();
+
+        SDL_GL_SwapWindow(window);
     }
 
     SDL_DestroyWindow(window);
+
     return 0;
 }
