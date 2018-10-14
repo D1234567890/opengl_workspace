451b6851ea17fbc5362d4c2b0a8359b1fe089a79
Stupid idiot <league@WTS-IM-6.attlocal.net>
2018-10-14 16:00:55 -0700

    DONE
    DONE

--- main.cpp
+++ main.cpp
@@ -1,8 +1,42 @@
 #define STB_IMAGE_IMPLEMENTATION
+#define PI 3.14159265358979323
 
 #include "SDL2/SDL.h"
 #include "OpenGL/gl.h"
 #include "stb_image.h"
+#include <math.h>
+
+void drawCircle(int x, int y, int r){
+        glClear(GL_COLOR_BUFFER_BIT);
+        glEnable(GL_MULTISAMPLE);
+
+        /*glTexCoord2f(0, 0);
+        glVertex2f(-1, 1);
+        glTexCoord2f(1, 0);
+        glVertex2f(1, 1);
+        glTexCoord2f(0, 1);
+        glVertex2f(-1, -1);
+        glTexCoord2f(0, 1);
+        glVertex2f(-1, -1);
+        glTexCoord2f(1, 1);
+        glVertex2f(1, -1);
+        glTexCoord2f(1, 0);
+        glVertex2f(1, 1);*/
+
+        glBegin(GL_TRIANGLE_FAN);
+
+        glTexCoord2f(x / 500, y / 500);
+        glVertex2f(x, y);
+        glTexCoord2f(x / 500, (y + r) / 500);
+        glVertex2f(x, y + r);
+
+        for (int i = 0; i < 360; i++) {
+            glTexCoord2f((x - r * cos(PI * (i + 270) / 180) + 1) / 2, (y + r * sin(PI * (i + 270) / 180) + 1) / 2);
+            glVertex2f(x + sin(i + 1) * r, y + cos(i + 1) * r);
+        }
+        
+        glEnd();
+}
 
 int main(int argc, char** argv){
 
@@ -16,21 +50,24 @@ int main(int argc, char** argv){
 
     bool isRunning = true;
 
-    //unsigned char texData[] = {255, 0, 0, 255, 0, 255, 0, 255, 0, 0, 255, 255, 0, 0, 255, 255, 255, 0, 0, 255, 0, 255, 0, 255, 0, 255, 0, 255, 0, 0, 255, 255, 255, 0, 0, 255};
+    unsigned char texData[] = {255, 0, 0, 255, 0, 255, 0, 255, 0, 0, 255, 255, 0, 0, 255, 255, 255, 0, 0, 255, 0, 255, 0, 255, 0, 255, 0, 255, 0, 0, 255, 255, 255, 0, 0, 255};
 
     int w, h, n;
-    unsigned char *texData = stbi_load("rat.jpg", &w, &h, &n, 0);
+    //unsigned char *texData = stbi_load("rat.jpg", &w, &h, &n, 0);
     unsigned int textureID;
 
     glGenTextures(1, &textureID);
     glBindTexture(GL_TEXTURE_2D, textureID);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 3, 3, 0, GL_RGBA, GL_UNSIGNED_BYTE, texData);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 3, 3, 0, GL_RGB, GL_UNSIGNED_BYTE, texData);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     glEnable(GL_TEXTURE_2D);
 
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0, 500, 0, 500, -1, 1);
 
     while(isRunning){
         while(SDL_PollEvent(&event)){
@@ -46,26 +83,7 @@ int main(int argc, char** argv){
                 }
             }
         }
-
-        glClear(GL_COLOR_BUFFER_BIT);
-        glEnable(GL_MULTISAMPLE);
-
-        glBegin(GL_TRIANGLES);
-        glTexCoord2f(0, 1);
-        glVertex2f(-1, 1);
-        glTexCoord2f(1, 1);
-        glVertex2f(1, 1);
-        glTexCoord2f(0, 0);
-        glVertex2f(-1, -1);
-        glTexCoord2f(0, 0);
-        glVertex2f(-1, -1);
-        glTexCoord2f(1, 0);
-        glVertex2f(1, -1);
-        glTexCoord2f(1, 1);
-        glVertex2f(1, 1);
-        
-        glEnd();
-
+        drawCircle(250, 250, 250);
         SDL_GL_SwapWindow(window);
     }
 
@@ -73,3 +91,4 @@ int main(int argc, char** argv){
 
     return 0;
 }
+
